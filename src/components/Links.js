3083abc22741eeb084881dcf93ba188ca2f12c6c
cherotich <cherohellen14@gmail.com>
2024-03-02 00:56:24 -0800

    props

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Links(github,linkedin) {
+    return (
+      <div>
+        <h3>Links</h3>
+        <a href={github}>Github</a>
+        <a href>{linkedin}</a>
+      </div>
+    );
+  };
+  export default Links;
